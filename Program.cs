bb4efead8e71428f16b30f7d53933b9649811c9b
codefather0labs <vladyslav@codefather.dev>
2026-01-31 02:02:29 +0200

    core

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using WhisperTranscriptor.App;
+
+namespace WhisperTranscriptor;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => WhisperTranscriptor.App.Program.Main(args);
+}
+
---
+++ src/WhisperTranscriptor.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WhisperTranscriptor.App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
